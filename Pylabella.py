cfd6f94adbf832696dd65ecafcae7a6c76b8d7d6
Mario Sanchez <mario.sanchez.m12@gmail.com>
2024-01-16 21:35:08 -0300

    Ejercicio completo

--- Pylabella.py
+++ Pylabella.py
@@ -33,3 +33,43 @@ def misma_sucursal(RUT1,RUT2,clientes):
 
 print(misma_sucursal('16034124-5','3333333-3',clientes))
 print(misma_sucursal('5436576-2','3333333-3',clientes))
+
+"""
+Escriba la funcion mayores_que(edad_minima,clientes), que devuelva una lista con los
+nombres de todos los clientes cuya edad sea mayor o igual al parametro edad_minima.
+"""
+def mayores_que(edad_minima,clientes):
+    lista = []
+    for rut,datos in clientes.items():
+        edad = datos[1]
+        if edad >= edad_minima:
+            lista.append(datos[0])
+    return lista
+
+
+print(mayores_que(50,clientes))
+print(mayores_que(25,clientes))
+
+"""
+Escriba la funcion es_cliente_vip(RUT, monto_compra,clientes), la cual recibe como
+parametro el RUT de un cliente, el monto_compra que representa el monto minimo para ser
+considerado cliente VIP y el diccionario clientes. Esta funcion debe retornar True si la suma
+de las 3 ultimas compras del cliente es superior o igual al  monto_compra, y False en caso
+contrario.
+"""
+
+def es_cliente_vip(RUT, monto_compra,clientes):
+    for rut,datos in clientes.items():
+        if RUT == rut:
+            compras = datos[3]
+            total = 0
+            for i in compras:
+                total = total + i
+            if total >= monto_compra:
+                return True
+            else:
+                return False
+
+print(es_cliente_vip('5436576-2',500,clientes))
+print(es_cliente_vip('3333333-3',500,clientes))
+print(es_cliente_vip('1234567-8',150,clientes))
