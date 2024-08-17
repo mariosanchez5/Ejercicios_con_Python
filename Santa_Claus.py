44e8c9f9e21a9d7cdb38fbebfa72957a4e8699e2
Mario Sanchez <mario.sanchez.m12@gmail.com>
2024-08-17 16:57:22 -0400

    Agregando ejercicio de logica en python

---
+++ Santa_Claus.py
@@ -0,0 +1,37 @@
+"""
+Santa Claus ha comprado un sistema hecho en Python para administrar la Navidad 2015.
+Basicamente, el sistema maneja una lista de tuplas, donde cada tupla corresponde a los registros 
+de una persona con el formato (nombre, (x, y), tipo), donde (x, y) son las coordenadas
+en el mapa de la ubicacion de la persona y tipo indicaba como se ha portado la persona ('B':
+Bueno; 'M': Malo). Ver ejemplo:
+"""
+
+personas = [('Erick Lopez', (12.9887, 1.5567), 'M'), ('Cesar Moltedo',(12.1986, 2.5321), 'M'), ('Andrew Ng', (-2.9001, 7.6453), 'M'), ('Zafradaa', (12.2316, 5.0089), 'B')]
+
+"""
+Nota: Asuma que cada nombre aparece solo una vez. 
+Ahora usted debe:
+"""
+
+"""
+Desarrolle la funcion persona_mas_cerca(personas, posicion) que reciba la lista personas
+y la posicion actual (posicion) de Santa Claus. La funcion debe retornar el nombre de la persona
+mas cercana a la posicion de Santa Claus. Para calcular esta distancia recuerde que, en R2
+, si se
+tiene un punto (x1, y1) y un punto (x2, y2), entonces la distancia entre los dos puntos viene dada
+por raiz[(x2 − x1)2 + (y2 − y1)2]
+"""
+
+def persona_mas_cerca(personas, posicion) :
+    min = 100000000
+    x1,y1 = posicion
+    for tupla in personas:
+        nombre, ubicacion, estado = tupla
+        x2,y2 = ubicacion
+        distancia = ((x2-x1)**2 + (y2-y1)**2)**(1/2)
+        if distancia < min:
+            min = distancia
+            nom = nombre 
+    return nom
+
+print(persona_mas_cerca(personas, (12.9676, 1.4991)))
